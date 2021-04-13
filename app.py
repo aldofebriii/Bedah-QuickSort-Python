722c3508ef1c25f1b90043cf1ca4a4464a455a82
aldofebriii <aldothepro@gmail.com>
2021-04-13 10:43:10 +0700

    update New Version

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import sort
 #Status Bertanya
 ask = True
 
@@ -14,42 +14,48 @@ def acceptUserInput():
             global ask
             ask = False
             quit()
-        listLength = int(listLength)
-        if listLength < 3:
-            print('Minimal data yang harus dimasukan adalah 3\n')
-            return acceptUserInput()
-        print('Apakah Saudara ingin List Random dari kami ? (Y/N)\n')
-        askUser = True
-        while askUser:
-                jawabanUser = input('Y/N..?')
-                if(jawabanUser == 'Y' or jawabanUser =='y'):
-                    unsortedList = list(np.random.randint(512, size= listLength))
-                    print('====================\n',unsortedList,'\n====================\n')
-                    import tampilan
-                    tampilan.main(unsortedList)
-                    print('====================\n',unsortedList,'\n====================\n')
-                    unsortedList = []
-                    askUser = False
-                elif(jawabanUser == 'N' or jawabanUser == 'n'):
-                    askAngka = True
-                    while askAngka:
-                        for x in range(listLength):
-                            try:
-                                print('Silahkan masukan angka yang ', x + 1)
-                                angka = int(input())
-                                if angka == None:
-                                    raise ValueError
-                                unsortedList.insert(len(unsortedList), angka)
-                                askUser = False
-                                askAngka = False
-                            except ValueError:
-                                print('Data yang Saudara masukan salah, silahkan ulang \n')
-                                acceptUserInput()
+        elif(listLength == 'pure'):
+            listPureLen = int(input('Masukan Angka'))
+            unsortedList = list(np.random.randint(512, size= listPureLen))
+            print('====================\n',unsortedList,'\n====================\n')
+            pureQuickSort = sort.PureQuickSort(unsortedList).mulai(0, len(unsortedList))
+            print('====================\n',unsortedList,'\n====================\n', 'Waktu yang dibutuhkan: ', pureQuickSort[1])
+            unsortedList = []
+        else:
+            listLength = int(listLength)
+            if listLength < 3:
+                print('Minimal data yang harus dimasukan adalah 3\n')
+                return acceptUserInput()
+            print('Apakah Saudara ingin List Random dari kami ? (Y/N)\n')
+            askUser = True
+            while askUser:
+                    jawabanUser = input('Y/N..?')
+                    if(jawabanUser == 'Y' or jawabanUser =='y'):
+                        unsortedList = list(np.random.randint(512, size= listLength))
                         print('====================\n',unsortedList,'\n====================\n')
                         import tampilan
                         tampilan.main(unsortedList)
-                        print('====================\n',unsortedList,'\n====================\n')
                         unsortedList = []
+                        askUser = False
+                    elif(jawabanUser == 'N' or jawabanUser == 'n'):
+                        askAngka = True
+                        while askAngka:
+                            for x in range(listLength):
+                                try:
+                                    print('Silahkan masukan angka yang ', x + 1)
+                                    angka = int(input())
+                                    if angka == None:
+                                        raise ValueError
+                                    unsortedList.insert(len(unsortedList), angka)
+                                    askUser = False
+                                    askAngka = False
+                                except ValueError:
+                                    print('Data yang Saudara masukan salah, silahkan ulang \n')
+                                    acceptUserInput()
+                            print('====================\n',unsortedList,'\n====================\n')
+                            import tampilan
+                            tampilan.main(unsortedList)
+                            unsortedList = []
     except ValueError:
         acceptUserInput()
 
--- sort.py
+++ sort.py
@@ -8,12 +8,14 @@ class QuickSort():
     def updateTampilan(self, element1, element2):
         import tampilan
         tampilan.update(self, element1, element2)
+
     #Mulai
     def mulai(self, kiri, kanan):
         self.startTime = time.time()
         self.algoritma(kiri, kanan - 1)
         waktuEksekusi = time.time() - self.startTime
         return self.item, waktuEksekusi
+
     #Swap elemen i dengan elemen J
     def ganti(self,kiri, kanan):
         #Menggant elemet di list[i], list[j] = list[j], list[i]
@@ -21,6 +23,7 @@ class QuickSort():
         self.updateTampilan(self.item[kiri], self.item[kanan])
         return None
     #Partisi Element, untuk mendapatkan titik tengah
+
     def partisi(self, kiri, kanan):
         i = kiri
         j = kanan
@@ -53,5 +56,46 @@ class QuickSort():
             self.algoritma(key, kanan)
     
     
-    
-    
+class PureQuickSort(QuickSort):
+    def __init__(self, item):
+        super().__init__(item)
+
+    def mulai(self, kiri, kanan):
+        self.startTime = time.time()
+        self.algoritma(kiri, kanan - 1)
+        waktuEksekusi = time.time() - self.startTime
+        return self.item, waktuEksekusi
+
+    def partisi(self, kiri, kanan):
+        i = kiri
+        j = kanan
+        titikTengah = self.item[(kiri + kanan) // 2]
+        print('--------------\n')
+        print('Element Kiri ', self.item[i])
+        print('ElementKanan', self.item[j])
+        print('Pembanding', titikTengah)
+        print('Nilai i ', self.item[i])
+        print('Nilai j', self.item[j])
+        while i <= j:
+            while self.item[i] < titikTengah:
+                i+= 1
+            while self.item[j] > titikTengah:
+                j-= 1
+            if(i <= j):
+                self.ganti(i, j)
+                i+= 1
+                j-= 1
+        return i
+    def ganti(self,kiri, kanan):
+        #Menggant elemet di list[i], list[j] = list[j], list[i]
+        self.item[kiri], self.item[kanan] = self.item[kanan], self.item[kiri]
+        return None
+
+    def algoritma(self, kiri, kanan):
+        if(len(self.item) > 1):
+            key = self.partisi(kiri, kanan)
+        print(self.item)
+        if(key - 1 > kiri ):
+            self.algoritma(kiri, key - 1)
+        if(key < kanan ):
+            self.algoritma(key, kanan)
